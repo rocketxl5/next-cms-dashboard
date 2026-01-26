84c9990c6f01e0bacff8d1cbd17d2c3a1a392899
Max Gaillard <maxgaillar@gmail.com>
2026-01-26 12:56:47 -0500

    Fixed DashboardSidebar wrong import

--- src/app/(protected)/dashboard/components/DashboardSidebar.tsx
+++ src/app/(protected)/dashboard/components/DashboardSidebar.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { SideSlot } from '@/components/layout-primitives';
-import { dashboardNav } from '../dashboardNav';
+import { dashboardNav } from '@/types/ui';
 
 export function DashboardSidebar() {
   const pathname = usePathname();
