3dbd22e3b6cc5122995d526108a52741e37fad63
Max Gaillard <maxgaillar@gmail.com>
2026-03-03 14:09:13 -0500

    feat: refactor RoleSelect component to use Select from UI library and remove theme dependency

--- src/app/(protected)/dashboard/components/select/RoleSelect.tsx
+++ src/app/(protected)/dashboard/components/select/RoleSelect.tsx
@@ -1,39 +1,31 @@
 import { AppRole } from '@/types/enums';
-import { useTheme } from '@/providers';
+import { Select } from '@/components/ui';
 
 interface RoleSelectProps {
   value: AppRole;
   handleChange: (role: AppRole) => void;
   assignableRoles: AppRole[];
+  disabled?: boolean;
 }
 
 export function RoleSelect({
   value,
   handleChange,
   assignableRoles,
+  disabled,
 }: RoleSelectProps) {
-  const { theme } = useTheme();
-  console.log(theme);
-
   return (
-    <div className="space-y-2">
-      <select
-        data-theme={theme}
-        className="
-
-    border border-zinc-300
-    w-full px-3 py-2 text-sm
-  "
-        name="role"
-        value={value}
-        onChange={(e) => handleChange(e.target.value as AppRole)}
-      >
-        {assignableRoles.map((role) => (
-          <option data-theme={theme} key={role} value={role}>
-            {role}
-          </option>
-        ))}
-      </select>
-    </div>
+    <Select
+      name="role"
+      value={value}
+      disabled={disabled}
+      onChange={(e) => handleChange(e.target.value as AppRole)}
+    >
+      {assignableRoles.map((role) => (
+        <option key={role} value={role}>
+          {role}
+        </option>
+      ))}
+    </Select>
   );
 }
