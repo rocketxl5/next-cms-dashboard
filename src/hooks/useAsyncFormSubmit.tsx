9cf4a30c517627472091fa12365c4d191c942b58
Max Gaillard <maxgaillar@gmail.com>
2026-04-01 11:31:04 -0400

    Added useAsyncFormSubmit hook template

---
+++ src/hooks/useAsyncFormSubmit.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import { useState } from "react";
+import { FieldValues, UseFormReturn } from "react-hook-form";
+
+import { useToast } from "@/providers";
+
+export function useAsyncFormSubmit<T extends FieldValues>() {
+    return null;
+}
