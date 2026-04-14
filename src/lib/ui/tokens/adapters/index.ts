b39030a69da96922076ed4da008b5de58389956f
Max Gaillard <maxgaillar@gmail.com>
2026-04-14 11:04:53 -0400

    Enhance UserForm layout; integrate Box and Grid components for improved structure and styling

--- src/lib/ui/tokens/adapters/index.ts
+++ src/lib/ui/tokens/adapters/index.ts
@@ -1,6 +1,7 @@
 export * from './border.adapters';
 export * from './color.adapters';
 export * from './radius.adapters';
+export * from './select.adapters';
 export * from './surface.adapters';
 export * from './text.adapters';
 export * as layoutAdapters from './layout';
